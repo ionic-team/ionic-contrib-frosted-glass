bc50234a5e1ab9eff0b2f8b2223f54cf035536bb
Max Lynch <max@drifty.com>
2014-01-31 15:04:24 -0600

    Slightly tweaked styles

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-ionic-contrib-frosted-glass
+Ionic Frosted Glass
 ===========================
 
 An optional frosted-glass effect for iOS 7 styled Ionic apps.
+
+To use, include `ionic.contrib.frostedGlass.css` and `ionic.contrib.frostedGlass.js` in your app.
+
+Then, apply the `frosted-bar` attribute directive to a `<header-bar>` or `<nav-bar>` to get the frosted effect. Note: you must also
+have a `<content>` directive on the page which is where the content will be taken from for the blur. Also, feel free to use
+the extra class `bar-frosted` for header bars that comes with the CSS, which gives you a light grey header bar much like
+iMessage:
+
+```html
+<body>
+  <pane ng-controller="PageCtrl">
+    <header-bar frosted-bar title="'Title'" type="bar-frosted"></header-bar>
+
+    <content has-header="true" has-footer="true" padding="true">
+      <ol class="messages">
+        <li ng-repeat="message in messages" ng-bind-html="message.content">
+        </li>
+      </ol>
+    </content>
+  </pane>
+</body>
+```
---
+++ www/js/ionic.contrib.frostedGlass.js
@@ -0,0 +1,79 @@
+(function() {
+
+'use strict';
+
+/**
+ * The ionic-contrib-frosted-glass is a fun frosted-glass effect
+ * that can be used in iOS apps to give an iOS 7 frosted-glass effect
+ */
+angular.module('ionic.contrib.frostedGlass', ['ionic'])
+
+.factory('$ionicFrostedDelegate', ['$rootScope', function($rootScope) {
+  return {
+    update: function() {
+      $rootScope.$emit('ionicFrosted.update');
+    }
+  }
+}])
+
+.directive('frostedBar', ['$timeout', '$rootScope', '$ionicScrollDelegate', function($timeout, $rootScope, $ionicScrollDelegate) {
+  var clone = function($element) {
+    var el = $element[0];
+
+    var content = $element.parent()[0].querySelector('.scroll');
+    if(!content) {
+      return;
+    }
+
+    // Get the top offset position for headers, etc. on this content area
+    var contentOffset = content.parentNode.offsetTop;
+
+    // Get the height of the header to know how much to offset the content blur
+    var elHeight = el.offsetHeight;
+
+    // Clone the content
+    var contentCloned = content.cloneNode(true);
+
+    // Append the cloned content into the blur box
+    var blurContent = document.createElement('div');
+    blurContent.className = 'ionic-contrib-blur';
+    blurContent.style.overflow = 'hidden';
+    blurContent.style.height = elHeight + 'px';
+    blurContent.appendChild(contentCloned);
+
+    content.parentNode.addEventListener('scroll', function(e) {
+      // Move the clone up as we scroll
+      contentCloned.style[ionic.CSS.TRANSFORM] = 'translate3d(0,' + (-e.detail.scrollTop + contentOffset) + 'px, 0)';
+    });
+
+    // Append the blur box into this element
+    $element.append(blurContent);
+    return blurContent;
+  };
+
+  return {
+    restrict: 'AC',
+    link: function($scope, $element, $attr, frosted) {
+      var blurContent = null;
+
+      $rootScope.$on('ionicFrosted.update', function() {
+        window.rAF(function() {
+          if(blurContent) {
+            blurContent.remove();
+            blurContent = clone($element);
+          }
+        });
+      });
+
+      // timeout so we allow child directives to
+      // render children
+      $timeout(function() {
+        window.rAF(function() {
+          blurContent = clone($element);
+        });
+      });
+    }
+  }
+}]);
+
+})();
